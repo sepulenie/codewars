39d7631b5c40eaa98d85d3cd2036aa6f09d7ca45
maxim reutov <maximreutov@gmail.com>
2022-11-01 21:26:34 +0300

    still making valid_braces

--- tests.py
+++ tests.py
@@ -1,7 +1,18 @@
 def valid_braces(string):
-    numbers_list_2 = [-3, 3]
+    numbers_list_2 = string
+    print("Лист - ", numbers_list_2)
     if numbers_list_2[0]>0:
+        print("Неправильный лист")
         return False
-    for number in numbers_list_2:
-        brace_1 = number
-    
+    else:    
+        for number_1 in range(len(numbers_list_2)-1):
+            print("Попытка №",number_1)
+            brace_1 = numbers_list_2[number_1]
+            for number_2 in range(number_1, numbers_list_2[number_1+i]):
+
+
+
+
+valid_braces([-3, 3])
+valid_braces([-3, -2, 2, 3])
+#valid_braces([-3, -2, 1, 2, 3])
