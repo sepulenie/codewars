ce7ccd312b09c20f1d4daf52f0f35693d5575770
maxim reutov <maximreutov@gmail.com>
2022-11-02 21:50:27 +0300

    Complete valid_braces

--- 2022/valid_braces.py
+++ 2022/valid_braces.py
@@ -1,31 +1,32 @@
 def valid_braces(string):
-    braces_dict_1 = {"{":-1, "[": -1, "(": -1, ")":1, "]": 1, "}": 1}
-    braces_dict_2 = {"{":-3, "[": -2, "(": -1, ")":1, "]": 2, "}": 3}
-    numbers_list_1 = []
     numbers_list_2 = []
+    braces_dict_2 = {"{":-3, "[": -2, "(": -1, ")":1, "]": 2, "}": 3}
     for brace in string:
-        numbers_list_1.append(braces_dict_1[brace])
         numbers_list_2.append(braces_dict_2[brace])
-    print(numbers_list_2)
-    if sum(numbers_list_1) != 0:
-        return False
-    else:
-        for number in numbers_list_2:
-            brace_1 = number
-            brace_2 = numbers_list_2[number+1]
-            
+    number_1 = 0
+    while number_1 <= len(numbers_list_2)-1:
+        if numbers_list_2[number_1]>0:
+            return False
+        else:
+            number_2 = number_1+1
+            while number_2 < len(numbers_list_2):
+                if numbers_list_2[number_1]+numbers_list_2[number_2] == 0:    
+                    del numbers_list_2[number_2]
+                    del numbers_list_2[number_1]
+                    if numbers_list_2 == []:
+                        return True
+                    else:
+                        number_1 += 0
+                        break
+                else:
+                    if number_2 >= len(numbers_list_2)-1:
+                        return False
+                    else:
+                        number_2 += 2
 
-        return True
-        
-
-          
-
-            
 
 
-print(valid_braces("{)")) #True
-print(valid_braces("{)]")) #False
-print(valid_braces("{}")) # True
-print(valid_braces("{}()[])")) #False
-print(valid_braces("(({{[[]]}}))")) #True
-print(valid_braces("())({}}{()][][")) #False 
+
+
+print(valid_braces("((){}())")) #True
+print(valid_braces("[(])")) #False
