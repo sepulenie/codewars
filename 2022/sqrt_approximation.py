903257e5d9457ff69dd7da0b31e05650ddeba705
maxim reutov <maximreutov@gmail.com>
2022-11-03 19:42:19 +0300

    did sqrt_approximation

--- 2022/sqrt_approximation.py
+++ 2022/sqrt_approximation.py
@@ -3,19 +3,16 @@ def sqrt_approximation(number):
     l = number
     for i in range(1, number):
         l = l - k
-        print(l,"-",k)
         if l-k > 1:
             k += 2
         else:
             if l != 0:
-                print(number, " = ",[i, i+1])
+                print(number, " - ", [i, i+1])
                 return [i, i+1]
             else:
-                print(number, " = ", i)
+                print(number, " - ", i)
                 return i
 
 
-sqrt_approximation(15)
-
-'''for i in range (1, 9):
-    sqrt_approximation(i)'''
+for i in range (1, 900):
+    sqrt_approximation(i)
